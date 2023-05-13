4e84498a412346424b400266b4dc4ebd71d6fd10
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-13 22:20:57 +0700

    Add Font Lato

--- app/src/main/java/com/example/mytodo/MainActivity.kt
+++ app/src/main/java/com/example/mytodo/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
