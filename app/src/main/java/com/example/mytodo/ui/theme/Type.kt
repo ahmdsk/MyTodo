2de72c0bec163fcd845fd4ccd2bf2fbcff81c36b
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-13 20:56:47 +0700

    config: set colors

--- app/src/main/java/com/example/mytodo/ui/theme/Type.kt
+++ app/src/main/java/com/example/mytodo/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.example.mytodo.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -9,11 +10,12 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.sp,
+        color = Color(0xFFFFFFFF)
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
